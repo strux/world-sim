931db19afe69b712a1033a71efca181b5b6a33f0
Ben Carney <ben@carnivorous.org>
2015-12-13 23:41:21 -0800

    Hacked in yOffset for spritesheet

--- js/map.js
+++ js/map.js
@@ -1,10 +1,12 @@
 var Map = function(width, height, seed, tileWidth, tileHeight) {
-  var self = this;
+  var self = map = this;
   this.width = width;
   this.height = height;
   this.seed = seed || .1
   this.tileWidth = tileWidth || 32;
   this.tileHeight = tileHeight || 32;
+
+  this.sealevel = 162;
 
   this.data = [];
   this.tileData = [];
@@ -13,122 +15,36 @@ var Map = function(width, height, seed, tileWidth, tileHeight) {
   this.noise = new Noise();
   this.noise.seed(this.seed);
 
-  var ocean = [
-    '......DDDD......',
-  	'....DDDDDDDD....',
-  	'..DDDDDDDDDDDD..',
-  	'DDDDDDDDDDDDDDDD',
-  	'DDDDDDDDDDDDDDDD',
-  	'..DDDDDDDDDDDD..',
-  	'....DDDDDDDD....',
-  	'......DDDD......',
-  ];
-
-  var shallows = [
-    '......EEEE......',
-  	'....EEEEEEEE....',
-  	'..EEEEEEEEEEEE..',
-  	'EEEEEEEEEEEEEEEE',
-  	'EEEEEEEEEEEEEEEE',
-  	'..EEEEEEEEEEEE..',
-  	'....EEEEEEEE....',
-  	'......EEEE......',
-  ];
-
-  var beach = [
-    '......8888......',
-  	'....88888888....',
-  	'..888888888888..',
-  	'8888888888888888',
-  	'8888888888888888',
-  	'..888888888888..',
-  	'....88888888....',
-  	'......8888......',
-  ];
-
-  var grass = [
-    '......BBBB......',
-  	'....BBBBBBBB....',
-  	'..BBBBBBBBBBBB..',
-  	'BBBBBBBBBBBBBBBB',
-  	'BBBBBBBBBBBBBBBB',
-  	'66BBBBBBBBBBBB66',
-  	'..66BBBBBBBB66..',
-  	'....66BBBB66....',
-  	'......6666......',
-  	'................',
-  ];
-
-  var vegetation = [
-    '........A.......',
-  	'........9.......',
-  	'.......AAA......',
-  	'........9.......',
-  	'......AAAAA.....',
-  	'......B999......',
-  	'....BAAAAAAA....',
-  	'..BBBB99999BBB..',
-  	'BBBBAAAAAAAAABBB',
-  	'BBBBB9999999BBBB',
-  	'66BAAAAAAAAAAA66',
-  	'6666BBB666BB6666',
-  	'..66BBBBBBBB66..',
-  	'....66BBBB66....',
-  	'......6666......',
-  	'................',
-  ];
-
-  var alpine = [
-    '................',
-    '................',
-    '................',
-    '......999.......',
-    '.....9FFF9......',
-    '....9FFFFF9.....',
-    '...9999FF119....',
-    '...9919911919...',
-    '..991999111999..',
-    '.99999911111119.',
-    '9991999911111119',
-    '9919999991191999',
-    '1199999911999911',
-    '1111999919991111',
-    '..111199991111..',
-    '....11111111....',
-  	'......1111......',
-  	'................',
-  	'................',
-  	'................',
-  	'................',
-  ];
-
-  var elevation = function(name, rgbaArr, paletteKey, texture, width, height, yOffset) {
-      this.name = name;
-      this.rgbaArr = rgbaArr;
-      this.paletteKey = paletteKey;
-      this.texture = texture;
+  var elevation = function(index, width, height) {
+      this.index = index;
       this.width = width || self.tileWidth;
       this.height = height || self.tileHeight;
-      this.yOffset = yOffset || 0;
+
   }
-  elevation.prototype.rgba = function() {
-    return 'rgba(' + this.rgbaArr.join(',') + ')';
+  elevation.prototype = {
+    setYOffset: function(n) {
+      var multiplier = (255 - map.sealevel) / 100,
+          offset = (Math.max(n, map.sealevel) - map.sealevel) * multiplier;
+      return offset * .04;
+    },
+  }
+
+  var SpriteIndicies = {
+    ocean:    0,
+    shallows: 1,
+    beach:    2,
+    grass:    3,
+    alpine:   4,
+    snow:     5,
   }
 
   this.elevations = [];
-  this.elevations[155] = new elevation('ocean', [0, 71, 165, 1], 'D', ocean);
-  this.elevations[162] = new elevation('shallows', [0, 101, 165, 1], 'E', shallows);
-  this.elevations[166] = new elevation('beach', [204, 177, 82, 1], '8', beach);
-  this.elevations[171] = new elevation('grass', [86, 125, 38, 1], 'B', grass);
-  this.elevations[190] = new elevation('trees', [86, 125, 38, 1], 'A', vegetation);
-  this.elevations[255] = new elevation('alpine', [152, 152, 152, 1], '1', alpine);
-  /*
-  this.elevations[161] = new elevation('shallows', [0, 101, 165, 1], 'E', shallows);
-  this.elevations[170] = new elevation('grass', [86, 125, 38, 1], 'B', grass);
-  this.elevations[190] = new elevation('trees', [86, 125, 38, 1], 'A', vegetation);
-  this.elevations[205] = new elevation('alpine', [152, 152, 152, 1], '1', alpine);
-  this.elevations[255] = new elevation('snow', [240, 240, 240, 1], '2', snow);
-  */
+  this.elevations[155] = new elevation(SpriteIndicies.ocean);
+  this.elevations[map.sealevel] = new elevation(SpriteIndicies.shallows);
+  this.elevations[166] = new elevation(SpriteIndicies.beach);
+  this.elevations[180] = new elevation(SpriteIndicies.grass);
+  this.elevations[190] = new elevation(SpriteIndicies.alpine);
+  this.elevations[255] = new elevation(SpriteIndicies.snow);
 
   this.sumOctaveOptions = {
     iterations: 8,
@@ -188,7 +104,9 @@ Map.prototype = {
   getElevation: function(value) {
     for(var l in this.elevations) {
       if (Math.min(value, l) == value) {
-        return this.elevations[l];
+        var e = JSON.parse(JSON.stringify(this.elevations[l]));
+        e.yOffset = this.elevations[l].setYOffset(value);
+        return e;
       }
     }
   },
